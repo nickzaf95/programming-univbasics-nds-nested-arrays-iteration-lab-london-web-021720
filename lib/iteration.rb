6f15b6b0f3662cb6af8391fa9b2b5fec23bab95f
Nicholas Zafiriou <nicholaszafiriou@gmail.com>
2020-02-12 19:26:54 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -41,7 +41,9 @@ def total_even_pairs(src)
   while row_index < src.count do
     if ((src[row_index][0]%2) + (src[row_index][1]%2)) == 0 
       arr = arr + src[row_index][0] + src[row_index][1]
+    end
     row_index += 1
+    
   end
   arr
   
